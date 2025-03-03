48b5e47c8531398507e9eda0161462ed3b94ea8b
Nicholas Gamolin <asr8gn@virginia.edu>
2025-03-02 22:10:13 -0500

    Initial Commit

---
+++ Focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by Nicholas Gamolin on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Nicholas Gamolin on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
