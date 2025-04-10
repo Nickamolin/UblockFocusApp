27c63a07751c4521c7fa5af8846de992c323710d
Nicholas Gamolin <asr8gn@virginia.edu>
2025-04-10 04:50:38 -0400

    attemptend to add functionality via Device Activity Monitor Extension, yet to make progress

--- ActivityMonitor/DeviceActivityMonitorExtension.swift
+++ ActivityMonitor/DeviceActivityMonitorExtension.swift
@@ -6,18 +6,45 @@
 //
 
 import DeviceActivity
+import SwiftData
+import _SwiftData_SwiftUI
+import ManagedSettings
+
+extension ManagedSettingsStore.Name {
+    static let ublock = Self("ublock")
+}
 
 // Optionally override any of the functions below.
 // Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
 class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+    
     override func intervalDidStart(for activity: DeviceActivityName) {
         super.intervalDidStart(for: activity)
+        
+        guard let modelContainer = try? ModelContainer(for: Schedule.self) else {return}
+        let descriptor = FetchDescriptor<DistractingApps>()
+        let context = ModelContext(modelContainer)
+        let distractingApps = try? context.fetch(descriptor)
+        
+        let uBlockStore = ManagedSettingsStore(named: .ublock)
+        
+        if !distractingApps!.isEmpty {
+            uBlockStore.shield.applications = distractingApps?.first?.selectionTokens
+        }
+        
+        print("interval started!")
         
         // Handle the start of the interval.
     }
     
     override func intervalDidEnd(for activity: DeviceActivityName) {
         super.intervalDidEnd(for: activity)
+        
+        let uBlockStore = ManagedSettingsStore(named: .ublock)
+        
+        uBlockStore.shield.applications = nil
+        
+        print("interval ended!")
         
         // Handle the end of the interval.
     }
@@ -45,4 +72,15 @@ class DeviceActivityMonitorExtension: DeviceActivityMonitor {
         
         // Handle the warning before the event reaches its threshold.
     }
+    
+//    @MainActor
+//    private func getScheduleData() -> [Schedule] {
+//        guard let modelContainer = try? ModelContainer(for: Schedule.self) else {
+//            return []
+//        }
+//        let descriptor = FetchDescriptor<Schedule>()
+//        let schedules = try? modelContainer.mainContext.fetch(descriptor)
+//        
+//        return schedules ?? []
+//    }
 }
